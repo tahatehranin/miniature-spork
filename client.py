969a8554fa24943476d95265dae4bfbfb69067a6
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2024-12-22 18:02:32 +0330

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 ip = "server"  # Use the service name defined in docker-compose
-port = 5230
+port = 4401
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 data = s.recv(1024)
